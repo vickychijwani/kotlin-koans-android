f7f7318431f423c6fd8c83345ea02a491de211e6
Vicky Chijwani <vickychijwani@gmail.com>
2017-06-11 17:38:40 +0530

    Disable ViewPager swiping + fix scrolling lag on Koan tab by using CodeFlask.

---
+++ app/src/main/code/me/vickychijwani/kotlinkoans/features/common/UnswipeableViewPager.kt
@@ -0,0 +1,22 @@
+package me.vickychijwani.kotlinkoans.features.common
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+class UnswipeableViewPager(context: Context?, attrs: AttributeSet?) : ViewPager(context, attrs) {
+
+    constructor(context: Context?) : this(context, null)
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+}
